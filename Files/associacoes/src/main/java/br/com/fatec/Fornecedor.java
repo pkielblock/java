cc8ed45c0330352d05adbaebb649f0f16af4d07b
pedro ☯️ <p.kielblock123@gmail.com>
2022-08-31 15:18:42 -0300

    Associaçoes

---
+++ Files/associacoes/src/main/java/br/com/fatec/Fornecedor.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.fatec;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Fornecedor {
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
+}
