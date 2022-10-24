517d3fe88d0df57bd1ed4ba5a28a8c5be1ce22ed
Pedro Kielblock <p.kielblock123@gmail.com>
2022-10-23 23:44:27 -0300

    Encapsulamento, Getter e Setters

--- Files/produtoEstoque/src/entities/Product.java
+++ Files/produtoEstoque/src/entities/Product.java
@@ -2,9 +2,9 @@ package entities;
 
 public class Product {
 
-        public String name;
-        public double price;
-        public int quantity;
+        private String name;
+        private double price;
+        private int quantity;
 
         public Product(String name, double price, int quantity) {
             this.name = name;
@@ -16,10 +16,29 @@ public class Product {
             this.name = name;
             this.price = price;
         }
-        
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+
+        public void setPrice(double price) {
+            this.price = price;
+        }
+
+        public int getQuantity() {
+            return quantity;
+        }
 
         public double totalValueInStock() {
-            return price * quantity;
+                return price * quantity;
         }
 
         public void addProducts(int quantity) {
