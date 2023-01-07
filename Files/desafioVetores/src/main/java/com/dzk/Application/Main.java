68dba8eaedb15d955155e88931ef5ae100e77605
Pedro Kielblock <p.kielblock123@gmail.com>
2023-01-06 22:14:27 -0300

    Desafio Vetor

---
+++ Files/desafioVetores/src/main/java/com/dzk/Application/Main.java
@@ -0,0 +1,41 @@
+package com.dzk.Application;
+
+import java.util.Scanner;
+import com.dzk.Entities.Student;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numberRooms;
+        do {
+            System.out.println("How many rooms will be rented?");
+            numberRooms = sc.nextInt();
+        } while (numberRooms > 10 || numberRooms < 1);
+
+        String name, email;
+        int room;
+
+        Student[] vect = new Student[10];
+
+        for (int i = 0; i < numberRooms; i++) {
+            System.out.printf("Rent #%d%n", i+1);
+            System.out.println("Name: ");
+            name = sc.next();
+            System.out.println("Email: ");
+            email = sc.next();
+            System.out.println("Room: ");
+            room = sc.nextInt();
+
+            vect[room] = new Student(name, email, room);
+        }
+
+        System.out.println("Busy rooms: ");
+        for (int i = 0; i < vect.length; i++) {
+            if (vect[i] != null) {
+                System.out.println(vect[i].getRoom() + ": " + vect[i].getName() + ", " + vect[i].getEmail());
+            }
+        }
+
+        sc.close();
+    }
+}
---
+++ Files/desafioVetores/src/main/java/com/dzk/Entities/Student.java
@@ -0,0 +1,37 @@
+package com.dzk.Entities;
+
+public class Student {
+    private String name;
+    private String email;
+    private int room;
+
+    public Student(String name, String email, int room) {
+        this.name = name;
+        this.email = email;
+        this.room = room;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getRoom() {
+        return room;
+    }
+
+    public void setRoom(int room) {
+        this.room = room;
+    }
+}
