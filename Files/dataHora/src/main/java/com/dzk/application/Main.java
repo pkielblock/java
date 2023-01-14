62f98acf88dbdfe9aa4d1761d2a10f690dc88359
Pedro Kielblock <p.kielblock123@gmail.com>
2023-01-14 05:32:49 -0300

    Time and dates

---
+++ Files/dataHora/src/main/java/com/dzk/application/Main.java
@@ -0,0 +1,24 @@
+package com.dzk.application;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Main {
+    public static void main(String[] args) {
+
+        LocalDate d01 = LocalDate.now();
+        LocalDateTime d02 = LocalDateTime.now();
+        Instant d03 = Instant.now();
+        LocalDate d04 = LocalDate.parse("2022-07-20");
+        LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
+
+        System.out.println(d01);
+        System.out.println(d02);
+        System.out.println(d03);
+        System.out.println(d04);
+        System.out.println(d05);
+
+
+    }
+}
