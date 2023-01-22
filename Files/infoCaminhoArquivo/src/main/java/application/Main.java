e3777e1b5eaa3a3b279ed791016c97697b9e5213
Pedro Kielblock <p.kielblock123@gmail.com>
2023-01-22 13:37:35 -0300

    File functions

---
+++ Files/infoCaminhoArquivo/src/main/java/application/Main.java
@@ -0,0 +1,21 @@
+package application;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("File Path: ");
+        String strPath = sc.nextLine();
+
+        File path = new File(strPath);
+
+        System.out.println("getName: " + path.getName());
+        System.out.println("getParent: " + path.getParent());
+        System.out.println("getPath: " + path.getPath());
+
+        sc.close();
+    }
+}
