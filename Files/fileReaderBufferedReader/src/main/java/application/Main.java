e6ba2d090c8c9ec6cb4e29c9d7dc5a9c7075844c
Pedro Kielblock <p.kielblock123@gmail.com>
2023-01-22 13:18:34 -0300

    try-with-resourcess

--- Files/fileReaderBufferedReader/src/main/java/application/Main.java
+++ Files/fileReaderBufferedReader/src/main/java/application/Main.java
@@ -7,13 +7,8 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         String path = "/tmp/in.txt";
-        FileReader fr = null;
-        BufferedReader br = null;
 
-        try {
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
-
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             String line = br.readLine();
 
             while(line != null) {
@@ -22,17 +17,6 @@ public class Main {
             }
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
-        } finally {
-            try {
-                if (br != null) {
-                    br.close();
-                }
-                if (fr != null) {
-                    fr.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 }
