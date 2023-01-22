6d99d1cc3dfb9e5f0e305b932b38db50a36a493d
Pedro Kielblock <p.kielblock123@gmail.com>
2023-01-22 19:46:11 -0300

    Bounded Wildcard types | super - extends

---
+++ Files/fixIssueWildcardType/src/main/java/application/Main.java
@@ -0,0 +1,29 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> myInts = Arrays.asList(1, 2, 3, 4);
+        List<Double> myDoubles = Arrays.asList(3.14, 6.28);
+        List<Object> myObjs = new ArrayList<Object>();
+
+        copy(myInts, myObjs);
+        printList(myObjs);
+        copy(myDoubles, myObjs);
+        printList(myObjs);
+    }
+
+    public static void copy(List<? extends Number> source, List<? super Number> destiny) {
+        destiny.addAll(source);
+    }
+
+    public static void printList(List<?> list) {
+        for (Object obj : list) {
+            System.out.print(obj + " ");
+        }
+        System.out.println();
+    }
+}
