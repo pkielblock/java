c19c751dc8648736f5db5c89abc9bcb5fada6486
Pedro Kielblock <p.kielblock123@gmail.com>
2023-01-22 17:29:50 -0300

    Generics

---
+++ Files/generics/src/main/java/application/Main.java
@@ -0,0 +1,29 @@
+package application;
+
+import java.util.Scanner;
+
+import services.PrintService;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        PrintService<String> ps = new PrintService<>();
+
+        System.out.print("How many values? ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            String value = sc.next();
+            ps.addValue(value);
+        }
+
+        ps.print();
+        String x = ps.first();
+        System.out.println("First: " + x);
+
+        sc.close();
+    }
+}
