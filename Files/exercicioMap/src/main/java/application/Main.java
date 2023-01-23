0623ef66bfb5d15fb3d6c71e14f5f9eafdcfad98
Pedro Kielblock <p.kielblock123@gmail.com>
2023-01-23 13:17:17 -0300

    Map

--- Files/exercicioMap/src/main/java/application/Main.java
+++ Files/exercicioMap/src/main/java/application/Main.java
@@ -3,28 +3,43 @@ package application;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.ParseException;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         Scanner sc = new Scanner(System.in);
 
+        Map<String, Integer> votes = new LinkedHashMap<>();
 
-        System.out.print("File Path: ");
+        System.out.print("File Path:  ");
         String path = sc.nextLine();
 
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path))) {
             String line = bufferedReader.readLine();
 
-            while(line != null) {
+            while (line != null) {
                 String[] fields = line.split(",");
+                String name = fields[0];
+                int count = Integer.parseInt(fields[1]);
 
+                if (votes.containsKey(name)) {
+                    int votesSoFar = votes.get(name);
+                    votes.put(name, count + votesSoFar);
+                } else {
+                    votes.put(name, count);
+                }
+
+                line = bufferedReader.readLine();
             }
-
+            for (String key : votes.keySet()) {
+                System.out.println(key + ": " + votes.get(key));
+            }
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
         }
-
         sc.close();
     }
 }
